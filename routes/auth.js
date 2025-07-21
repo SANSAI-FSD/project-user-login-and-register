01d6fe90d3d03931aadb10953414ccbd7e1c0e4a
SANSAI-FSD <SANSAISANSAI2003@GMAIL.COM>
2025-07-21 20:21:56 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) return res.status(400).json({ error: "User already exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashed });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ error: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: "Invalid password" });
+
+    res.status(200).json({ message: "Login successful", username: user.username });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log("✅ Server running at http://localhost:5000"));console.log("MongoDB conneted")
+  })
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
